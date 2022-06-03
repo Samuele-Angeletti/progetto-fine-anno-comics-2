b0c4342a07de24967f8b380b1ed837caf0e2372a
Samuele-Angeletti <104771865+Samuele-Angeletti@users.noreply.github.com>
2022-06-03 13:14:06 +0200

    Dockign1.2

--- Progetto Fine Anno Gruppo 2/Assets/Scripts/ArchimedesMiniGame/Module.cs
+++ Progetto Fine Anno Gruppo 2/Assets/Scripts/ArchimedesMiniGame/Module.cs
@@ -13,6 +13,7 @@ namespace ArchimedesMiniGame
         [SerializeField] BoxCollider2D m_ExternalCollider;
         [SerializeField] SpriteRenderer m_ExternalSprite;
         [SerializeField] GameObject m_MapParent;
+        [SerializeField] Transform m_DockingPivot;
         [Space(10)]
         [Header("Main settings")]
         [SerializeField] float m_Acceleration;
@@ -33,6 +34,8 @@ namespace ArchimedesMiniGame
         private bool m_Docked;
         private Vector2 m_MaxSpeedVector;
         private Vector2 m_CurrentAcceleration;
+        private DockingPoint m_DockingPoint;
+        private bool m_Docking;
         private void Awake()
         {
             m_Rigidbody = GetComponent<Rigidbody2D>();
@@ -50,47 +53,75 @@ namespace ArchimedesMiniGame
 
         private void FixedUpdate()
         {
-            if (m_Direction.magnitude != 0)
-                m_CurrentAcceleration += new Vector2(MathF.Abs(m_Direction.normalized.x), MathF.Abs(m_Direction.normalized.y)) * m_Acceleration * Time.fixedDeltaTime;
-            else
-                m_CurrentAcceleration = Vector2.zero;
-            
+            if (!m_Docking && !m_Docked)
+            {
+                if (m_Direction.magnitude != 0)
+                    m_CurrentAcceleration += new Vector2(MathF.Abs(m_Direction.normalized.x), MathF.Abs(m_Direction.normalized.y)) * m_Acceleration * Time.fixedDeltaTime;
+                else
+                    m_CurrentAcceleration = Vector2.zero;
 
-            m_Rigidbody.velocity += -m_Direction.normalized * m_Acceleration * Time.fixedDeltaTime;
-            m_Rigidbody.rotation += m_RotationDirection.x * Time.fixedDeltaTime * m_RotationSpeed;
+
+                m_Rigidbody.velocity += -m_Direction.normalized * m_Acceleration * Time.fixedDeltaTime;
+                m_Rigidbody.rotation += m_RotationDirection.x * Time.fixedDeltaTime * m_RotationSpeed;
+            }
         }
 
         private void Update()
         {
-            if(m_Direction != Vector2.zero || m_RotationDirection != Vector2.zero)
+            if (!m_Docked)
             {
-                UseBattery();
-            }
+                if (!m_Docking)
+                {
+                    if (m_Direction != Vector2.zero || m_RotationDirection != Vector2.zero)
+                    {
+                        UseBattery();
+                    }
 
-            if (m_Rigidbody.velocity.x >= m_MaxSpeed)
+                    if (m_Rigidbody.velocity.x >= m_MaxSpeed)
+                    {
+                        m_Rigidbody.velocity = new Vector2(m_MaxSpeed, m_Rigidbody.velocity.y);
+                    }
+
+                    if (m_Rigidbody.velocity.y >= m_MaxSpeed)
+                    {
+                        m_Rigidbody.velocity = new Vector2(m_Rigidbody.velocity.x, m_MaxSpeed);
+                    }
+
+                    if (ForwardCheckOfWall(Vector2.up, 0.6f, m_DockingSide.transform.position, m_DockingMask) && m_Rigidbody.velocity.magnitude < 5f)
+                    {
+                        m_DockingAttemptAvailable = true;
+                        GameManagerES.Instance.ActiveDockingAttemptButton(true);
+                    }
+                    else if (m_DockingAttemptAvailable)
+                    {
+                        m_DockingAttemptAvailable = false;
+                        GameManagerES.Instance.ActiveDockingAttemptButton(false);
+
+                    }
+
+                }
+                else if (m_Docking)
+                {
+                    m_DockingPivot.parent = null;
+                    Docking();
+                }
+
+
+                GameManagerES.Instance.UpdateSpeed(m_Rigidbody.velocity.magnitude, m_MaxSpeedVector.magnitude);
+                GameManagerES.Instance.UpdateAcceleration(m_CurrentAcceleration.magnitude, m_MaxSpeed);
+            }
+        }
+
+        private void Docking()
+        {
+            Vector3 distanceBefore = m_DockingPivot.position - transform.position;
+            m_DockingPivot.position = Vector3.Lerp(m_DockingPivot.position, m_DockingPoint.DockingPivot.position, 1 * Time.deltaTime);
+            transform.position = transform.position + ((m_DockingPivot.position - transform.position) - distanceBefore);
+
+            if (Vector3.Distance(m_DockingPivot.position, m_DockingPoint.DockingPivot.position) < 0.1f)
             {
-                m_Rigidbody.velocity = new Vector2(m_MaxSpeed, m_Rigidbody.velocity.y);
+                DockingComplete();
             }
-
-            if (m_Rigidbody.velocity.y >= m_MaxSpeed)
-            {
-                m_Rigidbody.velocity = new Vector2(m_Rigidbody.velocity.x, m_MaxSpeed);
-            }
-
-            if (ForwardCheckOfWall(Vector2.up, 0.6f, m_DockingSide.transform.position, m_DockingMask))
-            {
-                m_DockingAttemptAvailable = true;
-                GameManagerES.Instance.ActiveDockingAttemptButton(true);
-            }
-            else if(m_DockingAttemptAvailable)
-            {
-                m_DockingAttemptAvailable = false;
-                GameManagerES.Instance.ActiveDockingAttemptButton(false);
-
-            }
-
-            GameManagerES.Instance.UpdateSpeed(m_Rigidbody.velocity.magnitude, m_MaxSpeedVector.magnitude);
-            GameManagerES.Instance.UpdateAcceleration(m_CurrentAcceleration.magnitude, m_MaxSpeed);
         }
 
         public void StartEngine()
@@ -152,15 +183,19 @@ namespace ArchimedesMiniGame
                 DockingPoint d = raycastHits[i].collider.GetComponent<DockingPoint>();
                 if (d != null && d.IsActive)
                 {
-                    Debug.Log("Attracco completato");
-                    m_ExternalCollider.enabled = false;
-                    m_ExternalSprite.enabled = false;
-                    m_MapParent.SetActive(true);
-                    Stop();
-                    m_Docked = true;
-                    PubSub.PubSub.Publish(new DockingCompleteMessage());
+                    Debug.Log("AGGANCIO ESEGUITO");
+                    m_Rigidbody.freezeRotation = true;
+                    m_Rigidbody.freezeRotation = false;
+                    m_Rigidbody.velocity = Vector3.zero;
+                    m_Docking = true;
+                    m_DockingPoint = d;
+                    SwitcherSystem.SwitchDirection(m_DockingPoint.Orientation,
+                        () => transform.eulerAngles = new Vector3(0, 0, 180),
+                        () => transform.eulerAngles = new Vector3(0, 0, 0),
+                        () => transform.eulerAngles = new Vector3(0, 0, 270),
+                        () => transform.eulerAngles = new Vector3(0, 0, 90));
                 }
-                else if(!d.IsActive)
+                else if(d == null || !d.IsActive)
                 {
                     Debug.Log("ATTRACCO FALLITO. PUNTO D'ATTRACCO NON ATTIVO");
                 }
@@ -169,6 +204,18 @@ namespace ArchimedesMiniGame
                     Debug.Log("ATTRACCO FALLITO. DISTANZA MINIMA NON RAGGIUNTA");
                 }
             }
+        }
+
+        private void DockingComplete()
+        {
+            Debug.Log("ATTRACCO COMPLETATO");
+            m_Docking = false;
+            m_ExternalCollider.enabled = false;
+            m_ExternalSprite.enabled = false;
+            m_MapParent.SetActive(true);
+            Stop();
+            m_Docked = true;
+            PubSub.PubSub.Publish(new DockingCompleteMessage());
         }
 
         public override void Interact()
