f1955d11e11e994820f996c82f70680fa828830e
Samuele-Angeletti <104771865+Samuele-Angeletti@users.noreply.github.com>
2022-06-27 14:11:48 +0200

    Room Cameras and Bug Fix

--- Progetto Fine Anno Gruppo 2/Assets/Scripts/ArchimedesMiniGame/Archimedes.cs
+++ Progetto Fine Anno Gruppo 2/Assets/Scripts/ArchimedesMiniGame/Archimedes.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using PubSub;
 using Commons;
+using Cinemachine;
+using MainGame;
+
 namespace ArchimedesMiniGame
 {
     public class Archimedes : MonoBehaviour, ISubscriber
     {
         [SerializeField] SpriteRenderer m_ExternalSpriteRenderer;
+        [SerializeField] CinemachineVirtualCamera m_MyCameraOnPlayer;
         private SpriteTransition m_SpriteTransition;
         private void Awake()
         {
@@ -40,6 +44,11 @@ namespace ArchimedesMiniGame
             {
                 m_SpriteTransition.ActiveSpriteTransparencyTransition(m_ExternalSpriteRenderer, EDirection.Down);
             }
+        }
+
+        public void SetCameraOnGameManager()
+        {
+            GameManager.Instance.SetCameraOnPlayer(m_MyCameraOnPlayer);
         }
     }
 }
