a2a54a184a65c9d32de278b8016faee2e056699c
Samuele-Angeletti <104771865+Samuele-Angeletti@users.noreply.github.com>
2022-07-01 19:07:47 +0200

    Light On PacMan

--- Progetto Fine Anno Gruppo 2/Assets/Scripts/Platform/LightManager.cs
+++ Progetto Fine Anno Gruppo 2/Assets/Scripts/Platform/LightManager.cs
@@ -5,25 +5,47 @@ using PubSub;
 using ArchimedesMiniGame;
 using Commons;
 using MicroGame;
-
+using UnityEngine.Rendering.Universal;
 namespace MainGame
 {
-    public class LightManager : MonoBehaviour
+    public class LightManager : MonoBehaviour, ISubscriber
     {
         // MANAGES THE GLOBAL ILLUMINATION OF THE WORLD
+        [SerializeField] Light2D m_GlobalLight;
+        [SerializeField] int m_LayerOnPacMan;
 
-        
+        int m_GlobalLightLayer;
 
-        // Start is called before the first frame update
-        void Start()
+        private void Start()
         {
-
+            PubSub.PubSub.Subscribe(this, typeof(PlayPacManMessage));
+            PubSub.PubSub.Subscribe(this, typeof(GameOverMicroGameMessage));
         }
 
-        // Update is called once per frame
-        void Update()
+        public void OnDisableSubscribe()
         {
+            PubSub.PubSub.Unsubscribe(this, typeof(PlayPacManMessage));
+            PubSub.PubSub.Unsubscribe(this, typeof(GameOverMicroGameMessage));
+        }
 
+        public void OnPublish(IMessage message)
+        {
+            if(message is PlayPacManMessage)
+            {
+                m_GlobalLightLayer = m_GlobalLight.gameObject.layer;
+                m_GlobalLight.gameObject.layer = m_LayerOnPacMan;
+            }
+            else if(message is GameOverMicroGameMessage)
+            {
+                GameOverMicroGameMessage gameOverMicroGameMessage = (GameOverMicroGameMessage)message;
+                if(gameOverMicroGameMessage.Win)
+                    m_GlobalLight.gameObject.layer = m_GlobalLightLayer;
+            }
+        }
+
+        private void OnDestroy()
+        {
+            OnDisableSubscribe();
         }
     }
 }
